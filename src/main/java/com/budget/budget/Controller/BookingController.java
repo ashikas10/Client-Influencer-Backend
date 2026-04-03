138a2f91ed7f399a6fe3e537b3ed5934ce8635fc
Ashika S <ashikas842005@gmail.com>
2026-04-03 12:13:51 +0530

    fixed cors origin

--- src/main/java/com/budget/budget/Controller/BookingController.java
+++ src/main/java/com/budget/budget/Controller/BookingController.java
@@ -10,12 +10,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.budget.budget.Model.Booking;
 import com.budget.budget.Repository.BookingRepository;
 
+@RequestMapping("/api")
 @RestController
 @CrossOrigin(origins = "*")
 public class BookingController {
