e26582d38dd1321d2111ec22d140e0caf215fafc
Ashika S <ashikas842005@gmail.com>
2026-04-14 19:59:57 +0530

    Update Changes

--- src/main/java/com/budget/budget/Controller/ClientController.java
+++ src/main/java/com/budget/budget/Controller/ClientController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,10 +25,10 @@ public class ClientController {
     private ClientRepository clientRepository;
 
     @PostMapping("/save")
-    public String saveUser(Client u) { 
+    public String saveUser(@RequestBody Client u) { 
         clientRepository.save(u);
         return "Saved Successfully";
-    }
+}
     @GetMapping("/all")
     public List<Client> getAllClients(){
         return clientRepository.findAll();
